fe3fb38e4ba94fa15b0d5a260e2da1a42ca3cbf4
David E. Rubio <hola@daviderubio.com>
2022-04-06 16:27:46 +0200

    selectroi.py can now be used to find widths for dictionary for sections

--- selectroi.py
+++ selectroi.py
@@ -3,6 +3,7 @@
 # Run this code in Mac or PC to define the image regions of interest (ROI) of the test image taken in raspberry pi
 
 import cv2
+from matplotlib import pyplot as plt
 
 # Open the image
 im = cv2.imread("/Users/daviderubio/Desktop/Python_stuff/environments/ueiya_env/v2_test_photos/calibrate.jpg")
@@ -23,7 +24,10 @@ cv2.imwrite('/Users/daviderubio/Desktop/Python_stuff/environments/ueiya_env/imag
 
 print('Copy the following number sequence to watermeterreader.py: ' + str(roi[1])+ ':' + str(roi[1]+roi[3])+ ',' +str(roi[0])+ ':' +str(roi[0]+roi[2]))
 
-# Example of printed Result (878, 673, 236, 50)
+# Show image cropped to find widths for 'Dictionary for Sections'
+plt.imshow(cv2.cvtColor(im_cropped, cv2.COLOR_BGR2RGB))
+plt.title('Image Cropped'); plt.show()
+
 
 # Reference table of Roi
 

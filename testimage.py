7e356fe72ab7145b42a9b9b5350426aa613d423b
David E. Rubio <hola@daviderubio.com>
2022-04-06 15:28:57 +0200

    script to get test image in raspberry pi

---
+++ testimage.py
@@ -0,0 +1,23 @@
+# testimage.py
+
+# run in raspberry pi to get test image
+
+# Import packages
+
+from picamera import PiCamera
+from time import sleep
+
+# Define Camera
+camera = PiCamera()
+
+# Start camera, define ISO and resolution
+camera.start_preview()
+camera.iso = 800
+camera.resolution = (2028, 1520)
+
+# Camera warm-up time
+sleep(2)
+
+# Save image
+camera.capture('/home/pi/python/environments/ueiya_env/images/calibrate.jpg')
+camera.stop_preview()

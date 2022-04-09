8be0d81e524b6a4940c418fa1f07289fa6b3b0b4
David E. Rubio <hola@daviderubio.com>
2022-04-09 09:17:09 +0200

    Added option to rotate image in selectroi.py

--- selectroi.py
+++ selectroi.py
@@ -1,33 +1,50 @@
-# select_roi.py
+# selectroi.py
 
 # Run this code in Mac or PC to define the image regions of interest (ROI) of the test image taken in raspberry pi
+# Then use the output text for watermeterreader.py
 
-import cv2
+import cv2 as cv
 from matplotlib import pyplot as plt
 
+# Custom display function
+def cv_show(name, img):
+    cv.imshow(name, img)
+    cv.waitKey(0)
+
+n = 'image preview'
+
 # Open the image
-im = cv2.imread("images/calibrate.jpg")
+path = "images/calibrate.jpg"
+img = cv.imread(path)
+
+# Rotate image - add '0' in rotate_angle if no rotation is necessary
+(h, w) = img.shape[:2]
+rotate_center = (w / 2, h / 2)
+rotate_angle = -2
+rotate_scale = 1
+
+rotate_matrix = cv.getRotationMatrix2D(rotate_center, rotate_angle, rotate_scale)
+imgRotated = cv.warpAffine(img, rotate_matrix, (w, h))
+#cv_show(n, imgRotated)
 
 # Select image roi - User input required in GUI
-roi = cv2.selectROI(im)
+roi = cv.selectROI(imgRotated)
 
 print('ROI is: ' + str(roi))
 
-im_cropped = im[int(roi[1]):int(roi[1]+roi[3]),int(roi[0]):int(roi[0]+roi[2])]
+img_cropped = imgRotated[int(roi[1]):int(roi[1]+roi[3]),int(roi[0]):int(roi[0]+roi[2])]
 
-cv2.imshow("Cropped Image", im_cropped)
-cv2.waitKey(0)
+cv.imshow("Cropped Image", img_cropped)
 
-cv2.imwrite('images/cropped_image.jpg',im_cropped)
+cv.imwrite('images/cropped_image.jpg',img_cropped)
 
 # Print output ordered for watermeterreader.py
 
 print('Copy the following number sequence to watermeterreader.py: ' + str(roi[1])+ ':' + str(roi[1]+roi[3])+ ',' +str(roi[0])+ ':' +str(roi[0]+roi[2]))
 
 # Show image cropped to find widths for 'Dictionary for Sections'
-plt.imshow(cv2.cvtColor(im_cropped, cv2.COLOR_BGR2RGB))
+plt.imshow(cv.cvtColor(img_cropped, cv.COLOR_BGR2RGB))
 plt.title('Image Cropped'); plt.show()
-
 
 # Reference table of Roi
 
@@ -36,7 +53,7 @@ plt.title('Image Cropped'); plt.show()
 # roi 2 = 236
 # roi 3 = 50
 
-# Example text of what numbers to add to Main Code to crop image 
+# Example number sequence
 
 # [675:723, 878:1114]
 
